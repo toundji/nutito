d53e20d69971c69389b97f6aed1b9a5c5e37660d
Donald KANTI <donaldkant2000@gmail.com>
2022-05-04 17:19:47 +0100

    committed some changes

--- nutito-api/ormconfig.ts
+++ nutito-api/ormconfig.ts
@@ -10,7 +10,7 @@ const config: PostgresConnectionOptions = {
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    //ssl: { rejectUnauthorized: false },
+    ssl: { rejectUnauthorized: false },
     synchronize: false,
     entities: ["dist/**/*.entity{.ts,.js}"],
     migrations: ["dist/src/db/migrations/*.js"],
