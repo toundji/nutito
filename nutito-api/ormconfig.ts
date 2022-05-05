0ef8876c905c6aba6de22fe5b68904e492d0ec3d
Donald KANTI <donaldkant2000@gmail.com>
2022-05-05 11:43:26 +0100

    committed some changes

--- nutito-api/ormconfig.ts
+++ nutito-api/ormconfig.ts
@@ -4,13 +4,13 @@ import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConne
 
 const config: PostgresConnectionOptions = {
     type: 'postgres',
-    //url: process.env.DB_URL,
+    url: process.env.DB_URL,
     host: process.env.DATABASE_HOST,
     port: (process.env.DATABASE_PORT as any) as number,
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    //ssl: { rejectUnauthorized: false },
+    ssl: { rejectUnauthorized: false },
     synchronize: false,
     entities: ["dist/**/*.entity{.ts,.js}"],
     migrations: ["dist/src/db/migrations/*.js"],
