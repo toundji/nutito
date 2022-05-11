9473d0c5f90af1c02775a4bb4ebbb60df6c9991b
Donald KANTI <donaldkant2000@gmail.com>
2022-05-11 21:55:30 +0100

    committed some changes

--- nutito-api/src/accountancy/dtos/create-client-operation-type.dto.ts
+++ nutito-api/src/accountancy/dtos/create-client-operation-type.dto.ts
@@ -1,4 +1,20 @@
+import { IsNotEmpty, IsString, IsPositive, IsEnum } from "class-validator";
+import { OperationTypeEnum } from '../../utilities/enums/operation-type.enum';
 
 export class CreateClientOperationDto {
-    
+
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    description: string;
+
+    @IsEnum(OperationTypeEnum)
+    type: string;
+
+    @IsNotEmpty()
+    @IsPositive()
+    creator_id: number;
 }
