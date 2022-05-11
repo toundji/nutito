8b25f95ffd4a50819e61c9360a7b725a3e80a9ce
Donald KANTI <donaldkant2000@gmail.com>
2022-05-11 21:47:49 +0100

    committed some changes

--- nutito-api/src/accountancy/dtos/create-career.dto.ts
+++ nutito-api/src/accountancy/dtos/create-career.dto.ts
@@ -1,4 +1,12 @@
+import { IsPositive } from "class-validator";
 
 export class CreateCareerDto {
-    
+    @IsPositive()
+    agent_id: number;
+
+    @IsPositive()
+    company_id: number;
+
+    @IsPositive()
+    agent_role_id: number;
 }
