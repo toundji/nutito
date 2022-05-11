3749fc41b9f0acd48507030dbffd61eb6009da95
Donald KANTI <donaldkant2000@gmail.com>
2022-05-11 19:47:38 +0100

    committed some changes

--- nutito-web-app/src/app/app-routing.module.ts
+++ nutito-web-app/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardLandingComponent } from './components/dashboard-landing/dashboard-landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: DashboardLandingComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- nutito-web-app/src/app/app.module.ts
+++ nutito-web-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardLandingComponent } from './components/dashboard-landing/dashboard-landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    LandingComponent,
+    SidebarComponent,
+    DashboardLandingComponent
   ],
   imports: [
     BrowserModule,
