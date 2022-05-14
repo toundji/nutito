62fa611df1978f8870c37a8cfc4f7a3528197a02
Donald KANTI <donaldkant2000@gmail.com>
2022-05-14 18:41:57 +0100

    committed some changes

--- nutito-api/src/accountancy/entities/company.entity.ts
+++ nutito-api/src/accountancy/entities/company.entity.ts
@@ -45,9 +45,7 @@ export class Company extends Audit {
     @ManyToMany(type => WorkField, workfield => workfield.companies, { onDelete: "NO ACTION" })
     @JoinTable({ name: "companies_workfields" })
     workfields: WorkField[]; 
-    newCompany: Company[];
-    newCompany: Company[];
-
+    
     get license(): Licence {
         return this.licences[this.licences.length - 1];
     }
