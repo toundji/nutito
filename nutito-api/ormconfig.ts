d3fd0cf922f178011d2fd43e019ac1e638a96cc5
Donald KANTI <donaldkant2000@gmail.com>
2022-05-21 17:02:54 +0100

    committed some changes

--- nutito-api/ormconfig.ts
+++ nutito-api/ormconfig.ts
@@ -8,7 +8,7 @@ const ormconfig: any = {
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    ssl: { rejectUnauthorized: false },
+    //ssl: { rejectUnauthorized: false },
     synchronize: false,
     entities: ["dist/src/**/*.entity{.ts,.js}"],
     migrations: ["dist/src/db/migrations/*.js"],
