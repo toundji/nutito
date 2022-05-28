4e047e295240d2e7332e19ca4e26db897d45a221
Donald KANTI <donaldkant2000@gmail.com>
2022-05-28 13:04:54 +0100

    committed some changes

--- nutito-web-app/src/app/app-routing.module.ts
+++ nutito-web-app/src/app/app-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardLandingComponent } from './components/dashboard-landing/dashboard-landing.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: "",
-    component: DashboardLandingComponent
-  },
+    component: AppComponent,
+    children: [
+      {
+        path: "",
+        component: LandingComponent,
+        outlet: "child1"
+      },
+      {
+        path: "dashboard",
+        component: DashboardLandingComponent,
+        outlet: "child1"
+      },
+    ]
+  }
 ];
 
 @NgModule({
