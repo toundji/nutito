fba1ae80053a42004a081bd3245d428fbe3812f0
Donald KANTI <donaldkant2000@gmail.com>
2022-06-03 06:40:56 +0100

    committed some changes

--- nutito-api/src/accountancy/services/company.service.ts
+++ nutito-api/src/accountancy/services/company.service.ts
@@ -55,7 +55,7 @@ export class CompanyService {
       createCompanyDto.companyCategoryId,
     );
     const user = await this.userService.findOneByPhone(createCompanyDto.user_phone);
-    const agentRole: AgentRole = await this.agentRoleService.findOneByName("Directeur");
+    const agentRole: AgentRole = await this.agentRoleService.findOneByName("Directeur Général");
     Object.keys(createCompanyDto).forEach(
         (key) => {
             newCompany[key] = createCompanyDto[key];
