6fa7d3a6b9b758c9435bb0913200d90a3bb0c6ca
toundji <olatoundjiagbahoungba@gmail.com>
2022-07-04 13:00:57 +0100

    ....

--- nutito-api/ormconfig.ts
+++ nutito-api/ormconfig.ts
@@ -1,14 +1,14 @@
 import 'dotenv/config';
 
 const ormconfig: any = {
-  type: 'mysql',
-  //url: process.env.DATABASE_URL,
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT as any as number,
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  //ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false },
   synchronize: true,
   entities: ['dist/src/**/*.entity{.ts,.js}'],
   migrations: ['dist/src/db/migrations/*.js'],
